7cb5d702d6b3db7109564af2475c1d662abab540
AayushKP <ayushprokashyap@gmail.com>
2026-01-26 03:03:29 +0530

    fix: fix in prod

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,21 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
+import "dotenv/config";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const globalQueryClient = global as unknown as {
+  postgres: Pool;
+};
+
+const pool =
+  globalQueryClient.postgres ||
+  new Pool({
+    connectionString: process.env.DATABASE_URL!,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalQueryClient.postgres = pool;
+}
 
 export const db = drizzle(pool, { schema });
 
